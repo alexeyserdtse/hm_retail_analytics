d9d719a3109cb5628d4a14ad35af97c5cedea4b8
Alexey Serdtse <alexeyserdtse90@gmail.com>
2026-07-25 22:37:24 +0300

    fix: extend date spine to 2099

--- include/hm_dwh/models/dwh/dim_date.sql
+++ include/hm_dwh/models/dwh/dim_date.sql
@@ -2,7 +2,7 @@ with spine as (
     {{ dbt_utils.date_spine(
         datepart="day",
         start_date="cast('2018-09-01' as date)",
-        end_date="cast('2020-10-01' as date)"
+        end_date="cast('2100-01-01' as date)"
     ) }}
 )
 
